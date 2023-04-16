78efab37ad251dbf8370150c76021d117f5fd94d
akartje <akartje13@gmail.com>
2023-04-16 13:39:57 -0500

    now displays how much an ability increased the roll by

--- dicerollers/forms.py
+++ dicerollers/forms.py
@@ -6,7 +6,17 @@ OPTION_CHOICES=[
     ('ea', 'Elemental Adept'),
 ]
 
+CRIT_TYPE=[
+    ('a', 'Each die gets rolled an additional time'),
+    ('b', 'The total damage is multiplied by two'),
+    ('c', 'All dice roll maximum'),
+    ('d', 'All dice roll maximum, then get rolled again'),
+
+]
+
 class DiceRollForm(forms.Form):
     dice = forms.IntegerField(min_value=1, max_value=100, label='Number of dice to roll')
     sides = forms.IntegerField(min_value=1, max_value=100, label='Number of sides on each die')
     option = forms.CharField(label='Any additional effects on this roll?', widget=forms.Select(choices=OPTION_CHOICES))
+    #crit = forms.BooleanField(label='Is this a critical hit?')
+    #crit_type = forms.CharField(label='What critical damage formula do you use?', widget=forms.Select(choices=CRIT_TYPE))
--- dicerollers/views.py
+++ dicerollers/views.py
@@ -19,6 +19,8 @@ class DiceRollView(View):
             option = form.cleaned_data['option']
             dice_pool = []
             result_pool = []
+            amount_option_gained = 0
+
             for _ in range(dice):
                 roll = random.randint(1, sides)
                 dice_pool.append(roll)
@@ -29,7 +31,12 @@ class DiceRollView(View):
                     if (roll == 1):
                         roll = 2
                 result_pool.append(roll)
+            
+            for i in range(len(result_pool)):
+                amount_option_gained += (result_pool[i] - dice_pool[i])
+
             result = sum(result_pool)
+            zipped_pool = zip(dice_pool, result_pool)
             DiceRoll.objects.create(roll=roll, option=option, result=result)
             rolls = DiceRoll.objects.all().order_by('-timestamp')[:10]
             return render(request, 'dice_roller.html', {
@@ -40,8 +47,8 @@ class DiceRollView(View):
                 'dice': dice, 
                 'sides': sides, 
                 'option': option,
-                'dice_pool': dice_pool,
-                'result_pool': result_pool
+                'zipped_pool': zipped_pool,
+                'amount_option_gained': amount_option_gained
                 })
         
         else:
